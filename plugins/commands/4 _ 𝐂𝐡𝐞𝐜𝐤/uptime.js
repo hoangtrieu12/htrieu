1410dcfda2e067ffb7b99680dbd2f5544526eaf6
hoangtrieu12 <115207385+hoangtrieu12@users.noreply.github.com>
2024-01-09 19:11:57 +0700

    Add files via upload

---
+++ plugins/commands/4 _ 𝐂𝐡𝐞𝐜𝐤/uptime.js
@@ -0,0 +1,27 @@
+const config = {
+  name: "uptime",
+  aliases: ["upt"],
+  permissions: [2],
+  credits: "vml"
+};
+
+async function onCall({ message }) {
+  const uptimeInSeconds = process.uptime();
+  const hours = Math.floor(uptimeInSeconds / 3600);
+  const minutes = Math.floor((uptimeInSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeInSeconds % 60);
+
+  try {
+    const replyMessage = await message.reply(`== 𝐔𝐏𝐓𝐈𝐌𝐄 ==
+${hours} giờ: ${minutes} phút: ${seconds} giây  
+`);
+    console.log(replyMessage);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default {
+  config,
+  onCall,
+};
